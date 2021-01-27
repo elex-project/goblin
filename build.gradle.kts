1424b27c5499a3c4bd043ffd401098990b75b403
Elex <68489652+elex-project@users.noreply.github.com>
2021-01-27 16:06:03 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+buildscript {
+    repositories {
+        jcenter()
+        google()
+    }
+
+    dependencies {
+        classpath ("com.jaredsburrows:gradle-license-plugin:0.8.90")
+    }
+}
+plugins {
+    id("com.github.ben-manes.versions") version "0.36.0"
+}
